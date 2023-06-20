6552cfaab922fe5f17b9d9cb77ebda60a4399c8d
Tunde Subair <rahtunde@gmail.com>
2023-06-20 18:41:04 +0100

    Using Fuction Based View For Create Retrieve or List

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,4 +1,8 @@
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+# from django.http import Http404
+from django.shortcuts import get_object_or_404
 
 
 from .models import Product
@@ -27,12 +31,41 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
 
 product_detail_view = ProductDetailAPIView.as_view()
 
-class ProductListAPIView(generics.ListAPIView):
-    """
-    not using this method yet
-    """
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+# class ProductListAPIView(generics.ListAPIView):
+#     """
+#     not using this method yet
+#     """
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
 
 
-product_list_view = ProductListAPIView.as_view()
+# product_list_view = ProductListAPIView.as_view()
+
+
+@api_view(['GET', 'POST'])
+def product_alt_view(request, pk = None, **args):
+
+    method = request.method
+    
+    if method == "GET":
+        if pk is not None:
+            obj = get_object_or_404(Product, pk=pk)
+            data = ProductSerializer(obj, many=False).data
+            return Response(data)
+        
+        queryset = Product.objects.all()
+        data = ProductSerializer(queryset, many=True).data
+        return Response(data)
+    
+    if method == 'POST':
+
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            title = serializer.validated_data.get('title')
+            content = serializer.validated_data.get('content')or None 
+            if content is None:
+                content = title
+            serializer.save(content=content)
+            return Response(serializer.data)
+        return Response({"Invalid": "Invaid data"}, status=400)
+    
