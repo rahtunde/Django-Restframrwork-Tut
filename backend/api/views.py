7859649f1f7ddcbf5a92c0edb366d05037f55131
Tunde Subair <rahtunde@gmail.com>
2023-06-20 15:09:08 +0100

    Django RestFrameWorkView

--- backend/api/views.py
+++ backend/api/views.py
@@ -6,15 +6,15 @@ from products.models import Product
 from products.serializers import ProductSerializer
 
 
-@api_view(['GET','POST'])
+@api_view(['POST'])
 def api_home(request, **args):
     """
     DRF API VIEW
     """
-    instance = Product.objects.all().order_by("?").first()
-    data = {}
-    if instance:
-        # data = model_to_dict(instance, fields=['id', 'title', 'price', 'sale_price'])
-        data = ProductSerializer(instance).data
-    return Response(data)
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        # instance = serializer.save()
+        print(serializer.data)
+        return Response(serializer.data)
+    return Response({"Invalid": "Invaid data"}, status=400)
 
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,8 +4,8 @@ endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/"
 
 
-get_response = requests.get(endpoint, params={"product_id": 123}, json={"query": "Hello World"})
+get_response = requests.post(endpoint, json={"title": "abe123", "content": "hello world", "price": "asqw3"})
 # print(get_response.status_code)
 print(get_response.json())
-print(get_response.text)
+# print(get_response.text)
 
