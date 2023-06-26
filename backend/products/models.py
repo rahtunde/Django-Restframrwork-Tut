61cfca922584275bcf00750311922b6b1571d2f4
Tunde Subair <rahtunde@gmail.com>
2023-06-26 16:31:11 +0100

    Added Search

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,10 +1,38 @@
 from django.conf import settings
 from django.db import models
+from django.db.models.query import QuerySet
+from django.db.models import Q
 
 # Create your models here.
 
 
 User = settings.AUTH_USER_MODEL
+
+
+class ProductQuerySet(models.QuerySet):
+    def is_public(self):
+        return self.filter(public=True)
+    
+    def search(self, query, user=None):
+        lookup = Q(title__icontains=query)  | Q(content__icontains=query)
+        qs = self.is_public().filter(lookup)
+
+        if user is not None:
+            qs2 = self.filter(user=user).filter(lookup)
+            qs = (qs | qs2).distinct()
+        return qs
+
+
+class ProductManger(models.Manager):
+    def get_queryset(self, *args, **kwargs):
+        return ProductQuerySet(self.model, using=self._db)
+
+    def search(self, query, user=None):
+        return self.get_queryset().search(query, user=user)
+
+
+
+
 
 class Product(models.Model):
 
@@ -12,6 +40,9 @@ class Product(models.Model):
     title = models.CharField(max_length=120)
     content = models.TextField(null=True, blank=True)
     price = models.DecimalField(max_digits=15, decimal_places=2, default=99.99)
+    public = models.BooleanField(default=True)
+
+    objects = ProductManger()
 
 
     @property
